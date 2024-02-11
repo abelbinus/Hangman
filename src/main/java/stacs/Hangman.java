bf0f572294dba63615f2b5199b33ac917a562c5d
Abel Binu Jacob <abj5@st-andrews.ac.uk>
2024-02-11 20:55:54 +0000

    Added hangman display and logic code.

--- src/main/java/stacs/Hangman.java
+++ src/main/java/stacs/Hangman.java
@@ -1,7 +1,10 @@
 package stacs;
 public class Hangman {
     public static void main(String[] args) {
-        HangmanController hangmanController = new HangmanController(); 
+        HangmanController hangmanController = new HangmanController();
+        HangmanView hangmanView = new HangmanView();
+        hangmanController.setObject(hangmanView);
+        hangmanController.setGame();
         hangmanController.startGame();
     }
 
